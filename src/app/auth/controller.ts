1840bafd530d96cfc4fe69b8079edad095ec2230
Pushkar Adhikari <adhikari.1990@gmail.com>
2026-04-24 22:38:56 +0530

    sign-up route

---
+++ src/app/auth/controller.ts
@@ -0,0 +1,85 @@
+import type { Request, Response } from 'express';
+import { signinPayloadModel, signupPayloadModel } from './models'
+import { users } from '../../db/schema';
+import { db } from '../../db';
+import { createHmac, randomBytes } from 'node:crypto';
+import { eq } from 'drizzle-orm';
+
+class AuthenticationController {
+    public async handleSignup(req: Request, res: Response) {
+        const validationResult = await signupPayloadModel.safeParseAsync(req.body);
+        if (validationResult.error) {
+            return res.status(400).json({
+                message: "body validation failed!",
+                error: validationResult.error,
+            })
+        }
+
+        const { firstName, lastName, email, password } = validationResult.data;
+
+        const userEmailResult = await db.select().from(users).where(eq(users.email, email))
+
+        if (userEmailResult.length > 0) {
+            return res.status(400).json({
+                message: `User with email ${email} already exists!`
+            })
+        }
+
+        const salt = randomBytes(32).toString('hex');
+        const hash = createHmac('sha256', salt).update(password).digest('hex');
+
+        const [result] = await db.insert(users).values({
+            firstName,
+            lastName,
+            email,
+            password: hash,
+            salt
+        }).returning({ id: users.id })
+
+        return res.status(201).json({
+            message: 'User has been created successfully!',
+            data: { id: result?.id }
+        })
+    }
+
+    public async handleSignin(req: Request, res: Response) {
+        const validationResult = await signinPayloadModel.safeParseAsync(req.body);
+        if (validationResult.error) {
+            return res.status(400).json({
+                message: "body validation failed!",
+                error: validationResult.error,
+            })
+        }
+
+        const { email, password } = validationResult.data;
+
+        const [userSelect] = await db.select().from(users).where(eq(users.email, email));
+        if (!userSelect) return res.status(400).json({
+            message: `User with email ${email} does not found!`
+        })
+
+
+        const salt = userSelect.salt!;
+        const hash = createHmac('sha256', salt).update(password).digest('hex');
+
+        if (userSelect.password !== hash) return res.status(400).json({
+            message: 'email or password is incorrect!'
+        })
+
+        //TOKEN GENERATION LOGIC SHOULD BE HERE
+
+
+        return res.json({
+            message: 'User has been signed in successfully!',
+            data: { token: userSelect.id }
+        })
+
+        // return res.status(200).json({
+        //     message: 'User has been signed in successfully!',
+        //     data: { id: userSelect.id }
+        // })
+    }
+
+}
+
+export default AuthenticationController;
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,17 +1,21 @@
 import express from "express";
+import { authRouter } from "./auth/routes";
 
 export function createApplication() {
     const app = express();
 
 
     //Middleware
-
+    app.use(express.json());
 
 
     // Routes
     app.get('/', (req, res) => {
         return res.json({ message: 'Welcome to Auth Service' });
     })
+
+    app.post('/sign-up', authRouter)
+
 
     return app;
 }
